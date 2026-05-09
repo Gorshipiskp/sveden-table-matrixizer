1a97a53a826ad9801eb0630e9896718ff135db83
Gorshipisk <bestrp2021@gmail.com>
2026-05-09 15:58:16 +0300

    improved `pyproject.toml`, added some exports

--- sveden_table_matrixizer/__init__.py
+++ sveden_table_matrixizer/__init__.py
@@ -1,3 +1,7 @@
+from .def_funcs import MatrixizedTable
+from .errors import NoTableHeaderError, NoTableBodyError, MultiplyTableBodyError, MultiplyTableHeaderError
 from .tables_extractor import matrixize_tables_from_page
+from .types import ExtractorOptions
 
-__all__ = ["matrixize_tables_from_page"]
+__all__ = ["matrixize_tables_from_page", "MatrixizedTable", "NoTableHeaderError", "MultiplyTableHeaderError",
+           "NoTableBodyError", "MultiplyTableBodyError", "ExtractorOptions"]
